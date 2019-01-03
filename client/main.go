322bc5874d71b1dbc460e57799d46a345c82836f
Michel Casabianca <casa@sweetohm.net>
2019-01-03 23:43:03 +0100

    Added goroutines

--- client/main.go
+++ client/main.go
@@ -6,31 +6,61 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
 )
 
 const (
-	URL = "http://localhost:8080/"
+	URL = "http://localhost:"
+	Port = "8080"
+	Size = 2
 )
+
+func feed(count int, index chan int) {
+	for i := 1; i <= count; i++ {
+		index <- i
+	}
+	close(index)
+}
+
+func work(operation string, index chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case i, ok := <-index:
+			if ok {
+				response, err := http.Get(URL + Port + "/" + operation + "/" + strconv.Itoa(i))
+				if err != nil {
+					panic(err)
+				}
+				content, err := ioutil.ReadAll(response.Body)
+				if err != nil {
+					panic(err)
+				}
+				println(string(content))
+			} else {
+				println("Thread done")
+				return
+			}
+		}
+	}
+}
 
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Println("ERROR: pass url and count (such as actor and 200) on command line")
+		fmt.Println("ERROR: pass operation and count (such as actor and 200) on command line")
 		os.Exit(-1)
 	}
-	url := os.Args[1]
+	operation := os.Args[1]
 	count, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		panic(err)
 	}
-	for i := 1; i <= count; i++ {
-		response, err := http.Get(URL + url + "/" + strconv.Itoa(i))
-		if err != nil {
-			panic(err)
-		}
-		content, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			panic(err)
-		}
-		println(string(content))
+	var wg sync.WaitGroup
+	wg.Add(Size)
+	index := make(chan int)
+	go feed(count, index)
+	for i := 0; i < Size; i++ {
+		go work(operation, index, &wg)
 	}
+	wg.Wait()
 }
